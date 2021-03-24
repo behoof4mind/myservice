c2fc37e73da7c382aeeb113614e5039287cef1a3
Denis <denis.lavrushko@infobip.com>
2021-03-24 15:47:27 +0300

    actions fixing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
-variable "env_prefix" {}
-variable "is_temp_env" {}
-variable "max_ec2_instances" {}
-variable "min_ec2_instances" {}
-variable "mysql_username" {}
-variable "mysql_password" {}
+variable "env_prefix" { default="test" }
+variable "is_temp_env" { default=true }
+variable "max_ec2_instances" { default=1 }
+variable "min_ec2_instances" { default=1 }
+variable "mysql_username" { default="root" }
+variable "mysql_password" { default="mysqlpass" }
