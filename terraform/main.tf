6b434978f1bb1374dbcc0e474b18bc9271d548b2
Denis <denis.lavrushko@infobip.com>
2021-03-24 14:29:54 +0300

    added to terraform get path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,13 @@
+terraform {
+  backend "s3" {}
+}
+
 module "webserver_cluster" {
-  source = "github.com/behoof4mind/tf-module-myservice?ref=0.1.4"
-  env_prefix = var.env_prefix
-  is_temp_env = var.is_temp_env
+  source            = "github.com/behoof4mind/tf-module-myservice?ref=0.1.4"
+  env_prefix        = var.env_prefix
+  is_temp_env       = var.is_temp_env
   max_ec2_instances = var.max_ec2_instances
   min_ec2_instances = var.min_ec2_instances
-  mysql_username = var.mysql_username
-  mysql_password = var.mysql_password
+  mysql_username    = var.mysql_username
+  mysql_password    = var.mysql_password
 }
