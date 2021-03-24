623d60c09a61b31b81874e60b0522d3ec192bcf8
Denis <denis.lavrushko@infobip.com>
2021-03-24 16:09:19 +0300

    actions fixing

--- .github/actions/comment.js
+++ .github/actions/comment.js
@@ -1,20 +1,26 @@
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-async function run() {
-    const { issue_number, github_token, owner, repo, website_link } = process.env
-
-    const octokit = new github.GitHub(github_token);
-
+export async function run() {
     try {
-        const { data } = await octokit.issues.createComment({
-            owner,
-            repo,
-            issue_number,
+        const { issue_number, github_token, owner, repo, website_link } = process.env
+
+        if (github.context.payload.action !== 'opened') {
+            console.log('No issue or pull request was opened, skipping');
+            return;
+        }
+
+        const client: github.GitHub = new github.GitHub(github_token);
+        await client.issues.createComment({
+            owner: owner,
+            repo: repo,
+            issue_number: issue_number,
             body: `[Review website here](${website_link.replace(/"/gs, '')})`
         });
-
-    } catch (err) {
-        throw err
+    }
+    catch (error) {
+        core.setFailed(error.message);
+        throw error;
     }
 }
 
