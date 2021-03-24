9943b0391a6a9ff6feda36a65c3cbd4f07833524
Denis <denis.lavrushko@infobip.com>
2021-03-24 16:17:17 +0300

    actions fixing

--- .github/actions/comment.js
+++ .github/actions/comment.js
@@ -1,27 +1,51 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+const github = require('@actions/core');
 
-export async function run() {
+async function run() {
+    const { issue_number, github_token, owner, repo, website_link } = process.env
+
+    const octokit = new github.GitHub(github_token);
+
     try {
-        const { issue_number, github_token, owner, repo, website_link } = process.env
-
-        if (github.context.payload.action !== 'opened') {
-            console.log('No issue or pull request was opened, skipping');
-            return;
-        }
-
-        const client: github.GitHub = new github.GitHub(github_token);
-        await client.issues.createComment({
-            owner: owner,
-            repo: repo,
-            issue_number: issue_number,
+        const { data } = await octokit.issues.createComment({
+            owner,
+            repo,
+            issue_number,
             body: `[Review website here](${website_link.replace(/"/gs, '')})`
         });
-    }
-    catch (error) {
-        core.setFailed(error.message);
-        throw error;
+
+    } catch (err) {
+        throw err
     }
 }
 
 run();
+
+
+
+// import * as core from '@actions/core';
+// import * as github from '@actions/github';
+//
+// export async function run() {
+//     try {
+//         const { issue_number, github_token, owner, repo, website_link } = process.env
+//
+//         if (github.context.payload.action !== 'opened') {
+//             console.log('No issue or pull request was opened, skipping');
+//             return;
+//         }
+//
+//         const client: github.GitHub = new github.GitHub(github_token);
+//         await client.issues.createComment({
+//             owner: owner,
+//             repo: repo,
+//             issue_number: issue_number,
+//             body: `[Review website here](${website_link.replace(/"/gs, '')})`
+//         });
+//     }
+//     catch (error) {
+//         core.setFailed(error.message);
+//         throw error;
+//     }
+// }
+//
+// run();
