1918229a76447648f83da111b7ed09f5c8629752
Denis <denis.lavrushko@infobip.com>
2021-03-26 01:42:33 +0300

    changed module verison

--- models/migrations/01_init.sql
+++ models/migrations/01_init.sql
@@ -1,8 +1,6 @@
 -- +migrate Up
 CREATE database IF NOT EXISTS myservice;
 
-CREATE TABLE IF NOT EXISTS myservice.gorp_migrations (id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, applied_at TIMESTAMP);
-
 CREATE TABLE IF NOT EXISTS myservice.users (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name CHAR(100) UNIQUE,
