d472fa803de5b1cf2f265aaf2cc97010db6b9684
Denis <denis.lavrushko@infobip.com>
2021-03-26 00:42:20 +0300

    changed module verison

--- models/initial.go
+++ models/initial.go
@@ -13,7 +13,7 @@ var Db *sqlx.DB
 
 func init() {
 	environment.InitEnvVariables()
-	db, err := sql.Open("mysql", environment.E.DB_USERNAME+":"+environment.E.DB_PASSWORD+"@("+environment.E.DB_URL+")/mysql?parseTime=true")
+	db, err := sql.Open("mysql", environment.E.DB_USERNAME+":"+environment.E.DB_PASSWORD+"@("+environment.E.DB_URL+")/myservice?parseTime=true")
 	if err != nil {
 		logrus.Fatalf("Fail to connect to database: %v", err)
 	}
